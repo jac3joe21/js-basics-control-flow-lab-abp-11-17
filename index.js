1355f1e9589172cf601e5396438e42200dc9b2f6
Joseph Casiano <josephcasiano@gmail.com>
2017-11-15 22:47:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function scuberGreetingForFeet (feet) {
 }
 
 function ternaryCheckCity (city) {
-  return city === 'Yonkers' ? 'Ok, sounds great.' : 'No can do amigo!';
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
 function switchOnCharmFromTip (tip) {
